e3350c0d1ac043e77fea4a1ca43789baeb54dc3c
uapv2202351 <uapv2202351@e-mc136-08.ad.univ-avignon.fr>
2025-04-28 10:55:18 +0200

    ajout comparaison

---
+++ test.s
@@ -0,0 +1,40 @@
+			# This code was produced by the CERI Compiler
+	.text		# The following lines contain the program
+	.globl main	# The main function must be visible from outside
+main:			# The main function body :
+	movq %rsp, %rbp	# Save the position of the stack's top
+	push $6
+	push $7
+	push $8
+	push $9
+	pop %rbx
+	pop %rax
+	addq	%rbx, %rax
+	push %rax
+	pop %rbx
+	pop %rax
+	addq	%rbx, %rax
+	push %rax
+	push $6
+	push $5
+	pop %rbx
+	pop %rax
+	addq	%rbx, %rax
+	push %rax
+	pop %rbx
+	pop %rax
+	subq	%rbx, %rax
+	push %rax
+	pop %rbx
+	pop %rax
+	addq	%rbx, %rax
+	push %rax
+	push $1
+	pop %rbx
+	pop %rax
+	cmpq %rbx, %rax
+	sete %al
+	movzbq %al, %rax
+	push %rax
+	movq %rbp, %rsp		# Restore the position of the stack's top
+	ret			# Return from main function
